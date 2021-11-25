040321e60df3c9530dc40c245c429273b8d5d68e
Franklin David Macias Avellan <franklin.macias864@gmail.com>
2021-11-25 23:12:48 +0100

    Challenge 03 - Fibonacci

---
+++ Challenge-03-Fibonacci/js/index.js
@@ -0,0 +1,34 @@
+const buttonEl = document.querySelector(".calculate");
+let spanEl = document.querySelector("result");
+
+let f1 = 0, f2 = 0, f3 = 0;
+
+buttonEl.addEventListener("click", fibonacci);
+
+// Calculate the addition for the fibonnaci serie
+
+function fibonacci() {
+    let sum = 0;
+    let counter = -1;
+    do {
+        counter++;
+        if (counter == 0) {
+            f1 = 0;
+            console.log(f1);
+            continue;
+        } else if (counter == 1) {
+            f2 = 1
+            console.log(f2);
+            continue;
+        }
+        f3 = f1 + f2;
+        f1 = f2;
+        f2 = f3
+        if (f3 % 2 == 0) {
+            sum = sum + f3;
+        } 
+    } while (sum <= 4000000)
+    console.log(sum);
+    spanEl.textContent = sum;
+}
+
