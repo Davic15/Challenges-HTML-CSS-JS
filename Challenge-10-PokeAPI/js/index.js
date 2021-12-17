b9218fe784c49c6e065f8608f264733374fcf261
DESKTOP-A953543\Frank <franklin.macias864@gmail.com>
2021-12-18 00:52:54 +0100

    Challenge: PokeAPI

--- Challenge-10-PokeAPI/js/index.js
+++ Challenge-10-PokeAPI/js/index.js
@@ -3,7 +3,8 @@
 */
 const btnSearchPokemonEl = document.getElementById("btn-search-pokemon");
 const txtSearchPokemonEl = document.getElementById("txt-name-pokemon");
-
+const headerDescriptionEl = document.getElementById("description");
+const pokemonInformationEl = document.getElementById("pokemon-information");
 
 btnSearchPokemonEl.addEventListener("click", pokemonSearch);
 
@@ -12,9 +13,29 @@ function pokemonSearch() {
     console.log("hi")
 }
 
+function getImagePokemon (pokemonName) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+    .then (res => {
+        if(!res.ok){
+            throw Error("There is an error with the server");
+        } else {
+            return(res.json());
+        }
+    })
+    .then (data => {
+        console.log(data.sprites.front_default)
+        pokemonInformationEl.innerHTML += `
+        <p>${pokemonName}</p><br>
+        <img src="${data.sprites.front_default}" />
+        `;
+    })
+    .catch (err => {
+        console.log(err);
+    })
+}
 
 
-fetch("https://pokeapi.co/api/v2/pokemon/pikachu/")
+fetch("https://pokeapi.co/api/v2/pokedex/kanto")
     .then(res => {
         if(!res.ok){
             throw Error("There is an error with the server");
@@ -23,8 +44,12 @@ fetch("https://pokeapi.co/api/v2/pokemon/pikachu/")
         }
     })
     .then(data => {
-        console.log(data)
+        console.log(data);
+        headerDescriptionEl.innerText = data.descriptions[3].description;
+        for (const element of data.pokemon_entries){
+            getImagePokemon(element.pokemon_species.name);
+        }
     })
     .catch(err => {
         console.log(err);
-    })
+    });
