e6067e3426eaaf22717b9edb9acd29b9751e692f
DESKTOP-A953543\Frank <franklin.macias864@gmail.com>
2021-12-21 00:24:48 +0100

    Challenge 11 - OpenTripMap API and some small updates

--- Challenge-11-OpenTripMapAPI/js/index.js
+++ Challenge-11-OpenTripMapAPI/js/index.js
@@ -2,6 +2,8 @@
 const apiKey = "5ae2e3f221c38a28845f05b67528fbe4cbcfa02035fddf302b67610a";
 const btnSearch = document.getElementById("btn-search");
 const txtSearch = document.getElementById("search-text");
+const spanInformation = document.getElementById("information");
+const divPlaces = document.getElementById("places");
 
 /* Event listeners */
 btnSearch.addEventListener("click", searchCity);
@@ -14,19 +16,22 @@ function searchCity() {
         fetch(`http://api.opentripmap.com/0.1/en/places/geoname?name="${valueTxtSearch}"&apikey=${apiKey}`)
         .then(res => {
             if (!res.ok){
+                spanInformation.innerText = "There is an error with the server!";
                 throw Error("There is an error with the server");
             } else {
                 return res.json();
             }
         })
         .then (data => {
+            spanInformation.innerText = "Place found!";
             loadPlaces(data)
         })
         .catch(err => {
             console.log(err);
         });
     } else {
-        console.log("Type a city")
+        spanInformation.innerText = "Type a city";
+        console.log("Type a city!")
     }
 }
 
@@ -41,13 +46,35 @@ function loadPlaces(information) {
         }
     })
     .then (data => {
-        console.log(data)
+        console.log(data);
+        //console.log(data.features[1].properties.name);
+        for (const element of data.features){
+            console.log(element.properties.xid);
+            showPlaces(element.properties.xid);
+        }
     })
     .catch(err => {
         console.log(err);
     });
 }
 
-function showPlaces(xid) {
-
+function showPlaces(information) {
+    console.log(information)
+    fetch(`https://api.opentripmap.com/0.1/en/places/xid/${information}?apikey=${apiKey}`)
+    .then(res => {
+        if (!res.ok){
+            throw Error("There is an error with the server");
+        } else {
+            return res.json();
+        }
+    })
+    .then (data => {
+        console.log(data);
+        console.log(data.wikipedia_extracts);
+        console.log(data.wikipedia)
+        console.log(data.preview.source)
+    })
+    .catch(err => {
+        console.log(err);
+    })
 }
