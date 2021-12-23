94cb9ffcdcbeaf7d7eb3e4316dab408890c55d08
DESKTOP-A953543\Frank <franklin.macias864@gmail.com>
2021-12-24 00:28:44 +0100

    Challenge 13 - DotaAPI some updates

--- Challenge-13-DotaAPI/js/index.js
+++ Challenge-13-DotaAPI/js/index.js
@@ -1,5 +1,5 @@
 
-const divEl = document.getElementById("nuevo");
+const divEl = document.getElementById("container-heroes");
 
 fetch("https://api.opendota.com/api/constants/hero_names")
 //fetch("https://api.opendota.com/api/heroes/")
@@ -14,11 +14,81 @@ fetch("https://api.opendota.com/api/constants/hero_names")
     })
     .then(data => {
         let img = "https://api.opendota.com" + data.npc_dota_hero_abaddon.img;
-        divEl.innerHTML = `
-            <h3>${data.npc_dota_hero_abaddon.localized_name}</h3>
-            <p>Type: ${data.npc_dota_hero_abaddon.attack_type}</p>
-            <img src="${img}" alt="${data.npc_dota_hero_abaddon.localized_name}" />
+        divEl.innerHTML += `
+            <div class="card">
+                <div class="header">
+                    <h3 class="hero-name">${data.npc_dota_hero_abaddon.localized_name}</h3>
+                </div>
+                <div class="container">
+                <div>
+                <img src="${img}" alt="${data.npc_dota_hero_abaddon.localized_name}" />
+                </div>
+                    
+                    <p>Type Hero: ${data.npc_dota_hero_abaddon.attack_type}</p>
+                    <p>Attack: ${data.npc_dota_hero_abaddon.attack_type}</p>
+                    <p>Defense: ${data.npc_dota_hero_abaddon.attack_type}</p>
+                    <p>Mobility: ${data.npc_dota_hero_abaddon.move_speed}</p>
+                    <p>Roles: ${data.npc_dota_hero_abaddon.roles.join(", ")}</p>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="header">
+                    <h3 class="hero-name">${data.npc_dota_hero_abaddon.localized_name}</h3>
+                </div>
+                <div class="container">
+                    <img src="${img}" alt="${data.npc_dota_hero_abaddon.localized_name}" />
+                    <p>Type Hero: ${data.npc_dota_hero_abaddon.attack_type}</p>
+                    <p>Attack: ${data.npc_dota_hero_abaddon.attack_type}</p>
+                    <p>Defense: ${data.npc_dota_hero_abaddon.attack_type}</p>
+                    <p>Mobility: ${data.npc_dota_hero_abaddon.move_speed}</p>
+                    <p>Roles: ${data.npc_dota_hero_abaddon.roles.join(", ")}</p>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="header">
+                    <h3 class="hero-name">${data.npc_dota_hero_abaddon.localized_name}</h3>
+                </div>
+                <div class="container">
+                    <img src="${img}" alt="${data.npc_dota_hero_abaddon.localized_name}" />
+                    <p>Type Hero: ${data.npc_dota_hero_abaddon.attack_type}</p>
+                    <p>Attack: ${data.npc_dota_hero_abaddon.attack_type}</p>
+                    <p>Defense: ${data.npc_dota_hero_abaddon.attack_type}</p>
+                    <p>Mobility: ${data.npc_dota_hero_abaddon.move_speed}</p>
+                    <p>Roles: ${data.npc_dota_hero_abaddon.roles.join(", ")}</p>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="header">
+                    <h3 class="hero-name">${data.npc_dota_hero_abaddon.localized_name}</h3>
+                </div>
+                <div class="container">
+                    <img src="${img}" alt="${data.npc_dota_hero_abaddon.localized_name}" />
+                    <p>Type Hero: ${data.npc_dota_hero_abaddon.attack_type}</p>
+                    <p>Attack: ${data.npc_dota_hero_abaddon.attack_type}</p>
+                    <p>Defense: ${data.npc_dota_hero_abaddon.attack_type}</p>
+                    <p>Mobility: ${data.npc_dota_hero_abaddon.move_speed}</p>
+                    <p>Roles: ${data.npc_dota_hero_abaddon.roles.join(", ")}</p>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="header">
+                    <h3 class="hero-name">${data.npc_dota_hero_abaddon.localized_name}</h3>
+                </div>
+                <div class="container">
+                    <img src="${img}" alt="${data.npc_dota_hero_abaddon.localized_name}" />
+                    <p>Type Hero: ${data.npc_dota_hero_abaddon.attack_type}</p>
+                    <p>Attack: ${data.npc_dota_hero_abaddon.attack_type}</p>
+                    <p>Defense: ${data.npc_dota_hero_abaddon.attack_type}</p>
+                    <p>Mobility: ${data.npc_dota_hero_abaddon.move_speed}</p>
+                    <p>Roles: ${data.npc_dota_hero_abaddon.roles.join(", ")}</p>
+                </div>
+            </div>
         `
+       
         console.log(data);
         console.log(data.npc_dota_hero_abaddon.localized_name)
         console.log(data.npc_dota_hero_abaddon.attack_type)
